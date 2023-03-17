98302cf76ce6fb001449e8cdda1e6124e50434e3
SOL CHOI <sori_127@naver.com>
2023-03-17 11:28:43 +0900

    Update usage.tsx

--- usage.tsx
+++ usage.tsx
@@ -5,7 +5,7 @@ const Usage = () => {
   const {theme, colors} = useTheme();
 
   return (
-    <View style={{backgroundColor: colors.background}}>  // ✨ How to Use
+    <View style={{backgroundColor: colors.background}}>  // How to Use ✨
       {/* ... */}
     </View>
   );
